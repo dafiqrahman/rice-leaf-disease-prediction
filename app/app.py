08aef7b985b9ed1876aad058d3e9be066f510bd8
dafiqrahman <syairdafiqfr@students.undip.ac.id>
2023-11-03 14:54:54 +0700

    update app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from functions.predict import predict
 from PIL import Image
 from functions.disease_desc import disease_desc
 import os
-#make it center
+# make it center
 st.markdown("""
 <style>
 .center {
@@ -22,34 +22,41 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
+st.image("app/logo app.png")
 st.markdown('<h2 class="center"> Aplikasi Deteksi Penyakit pada Tanaman Padi </h2>',
             unsafe_allow_html=True)
+
 
 @st.cache_resource
 def load_model():
     model = keras.models.load_model('app/mobilenetv2')
     return model
+
+
 model = load_model()
 # Add a selectbox to the sidebar:
 img_input = st.file_uploader("Upload Image", type=["jpg", "png"])
 
 if img_input is not None:
-    #make image centered
-    col1,col2,col3 = st.columns([1,2,1])
+    # make image centered
+    col1, col2, col3 = st.columns([1, 2, 1])
     with col1:
         st.write("")
     with col2:
         st.image(img_input, width=300)
     with col3:
         st.write("")
-    #img_input to image
+    # img_input to image
     image = Image.open(img_input)
-    prediction,class_name = predict(model,image)
+    prediction, class_name = predict(model, image)
     st.markdown(prediction, unsafe_allow_html=True)
     st.markdown(
         f"<h3> <br> Deskripsi {class_name}", unsafe_allow_html=True)
-    description,solution = disease_desc(class_name)
+    description, solution, reference = disease_desc(class_name)
     st.markdown(description, unsafe_allow_html=True)
     st.markdown(
         f"<h3> <br> Solusi", unsafe_allow_html=True)
     st.markdown(solution, unsafe_allow_html=True)
+    st.markdown(
+        f"<h3> <br> Referensi", unsafe_allow_html=True)
+    st.markdown(reference, unsafe_allow_html=True)
--- app/functions/disease_desc.py
+++ app/functions/disease_desc.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
 
+
 def disease_desc(disease):
     print(os.getcwd())
-    data = pd.read_csv('app/disease_data/disease_data.csv',sep = ";")
+    data = pd.read_csv('app/disease_data/disease_data.csv', sep=";")
     data = data[data['disease'] == disease]
     print(data.description)
     description = data['description'].values[0]
@@ -11,4 +12,8 @@ def disease_desc(disease):
     solution = solution.split('|')
     solution = [f'<li>{sol}</li>' for sol in solution]
     solution = ''.join(solution)
-    return description, solution
+    reference = data['reference'].values[0]
+    reference = reference.split('|')
+    reference = [f'<li>{ref}</li>' for ref in reference]
+    reference = ''.join(reference)
+    return description, solution, reference
